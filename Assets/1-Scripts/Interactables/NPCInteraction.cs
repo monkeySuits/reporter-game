5785a169f5124717fb0724f21464821900d85c76
devlog98 <devlog98@outlook.com>
2021-12-04 14:57:26 -0300

    [ADD] NPC Interaction script

---
+++ Assets/1-Scripts/Interactables/NPCInteraction.cs
@@ -0,0 +1,23 @@
+using devlog98.Backdoor;
+using MSuits.Cutscene;
+using MSuits.Dialogue;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NPCInteraction : MonoBehaviour, IMouse {
+    [Header("Cutscene")]
+    [SerializeField] private Cutscene cutscene;
+
+    public void OnMouseDown(InputAction.CallbackContext context) {
+        if (!cutscene.HasStarted) {
+            cutscene.StartCutscene();
+        }
+        else {
+            DialogueManager.instance.NextSentence();
+        }
+    }
+
+    // unused methods
+    public void OnMouseDrag(InputAction.CallbackContext context) { }
+    public void OnMouseUp(InputAction.CallbackContext context) { }
+}
