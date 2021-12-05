79645a7819701ceee4cd03a3dfcdf294162c8e74
Daniela Arcanjo <44294286+MichaelDouglasPIX@users.noreply.github.com>
2021-12-04 21:53:58 -0300

    Goals
    Prefab de objetivos

---
+++ Assets/1-Scripts/ScriptableObjeto/Goals.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Goals", menuName = "Goal")]
+public class Goals : ScriptableObject
+{
+    public Sprite artworkImage;
+    public Sprite artworkImageChecked;
+}
---
+++ Assets/1-Scripts/ScriptableObjeto/GoalsDisplay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GoalsDisplay : MonoBehaviour
+{
+    [Header("Goal")]
+    public string goalText;
+    [Header("Gameobjects")]
+    public Goals goals;
+    public TextMeshProUGUI goal;
+    public Image artworkImage;
+
+    private bool check;
+
+    void Start()
+    {
+        goal.text = goalText;
+        artworkImage.sprite = goals.artworkImage;
+        Invoke("Checked",2);
+    }
+
+    void Checked(){
+        artworkImage.sprite = goals.artworkImageChecked;
+        check = true;
+        CheckedEffect();
+    }
+
+    void CheckedEffect(){
+        Color Image = artworkImage.color;
+        Color text = goal.color;
+        Image.a = 1f;
+        text.a = 1f;
+        artworkImage.color = Image;
+        goal.color = text;
+    }
+}
