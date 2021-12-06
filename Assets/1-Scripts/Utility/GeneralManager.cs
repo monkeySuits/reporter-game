625b082fca149375b3cf443354a58fd547846158
devlog98 <devlog98@outlook.com>
2021-12-06 02:22:48 -0300

    [ADD] GM added

---
+++ Assets/1-Scripts/Utility/GeneralManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Responsible for managing general game workflow
+ */
+
+namespace devlog98.Backdoor {
+
+#pragma warning disable 649
+
+    public class GeneralManager : MonoBehaviour {
+        public static GeneralManager instance; // singleton reference
+
+        private bool isLoading; // checks for loading in process
+
+        // singleton setup
+        private void Awake() {
+            if (instance != null && instance != this) {
+                Destroy(this.gameObject);
+            }
+            else {
+                instance = this;
+            }
+        }
+
+        // load scene
+        public void LoadScene(string sceneName) {
+            if (!isLoading) {
+                isLoading = true;
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
