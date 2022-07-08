5704645d296bb080d0675f6bba561dee5b31b47c
devlog98 <dno1998@hotmail.com>
2022-07-08 09:44:35 -0300

    [MOD] Cutscene Branch using Save Manager to play cutscenes only once

--- Assets/1-Scripts/Cutscene/Cutscene Units/CutsceneBranch.cs
+++ Assets/1-Scripts/Cutscene/Cutscene Units/CutsceneBranch.cs
@@ -1,4 +1,5 @@
 using devlog98.Backdoor;
+using MoreMountains.Tools;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,23 @@ using UnityEngine;
  * Branches main cutscene on multiple different cutscenes, based on Collectable Items the Player is carrying
  * 
  * The order of check is the same as in the Inspector, so you need to put the default cutscene (one without conditions) as the last one
+ * The exception is when you want a cutscene to always play first. Then you put it as the first one and fill the Play Only Once Flag for it to play ignoring conditions (only once)
  */
 
 namespace MSuits.Cutscene.Unit {
     public class CutsceneBranch : CutsceneUnit {
         [SerializeField] private List<CutsceneBranchPath> cutscenePaths;
-        private Cutscene currentCutscene;
+        private CutsceneBranchPath currentPath;
 
         public override void ExecuteStart() {
             bool unlockedCutscene = false;
             foreach (CutsceneBranchPath cutscenePath in cutscenePaths) {
                 unlockedCutscene = true;
+
+                if (cutscenePath.LoadProgression()) {
+                    unlockedCutscene = false;
+                    continue;
+                }
 
                 foreach (CollectableItem key in cutscenePath.keys) {
                     if (!PlayerInventory.instance.Inventory().Contains(key)) {
@@ -27,13 +34,13 @@ namespace MSuits.Cutscene.Unit {
                 }
 
                 if (unlockedCutscene) {
-                    currentCutscene = cutscenePath.cutscene;
+                    currentPath = cutscenePath;
                     break;
                 }
             }
 
-            if (currentCutscene != null) {
-                currentCutscene.StartCutscene();
+            if (currentPath != null) {
+                currentPath.cutscene.StartCutscene();
             }
             else {
                 isFinished = true;
@@ -41,7 +48,8 @@ namespace MSuits.Cutscene.Unit {
         }
 
         public override void ExecuteUpdate() {
-            if (currentCutscene != null && !currentCutscene.hasStarted) {
+            if (currentPath.cutscene != null && !currentPath.cutscene.hasStarted) {
+                currentPath.SaveProgression();
                 isFinished = true;
             }
         }
@@ -51,12 +59,44 @@ namespace MSuits.Cutscene.Unit {
 
         /*
          * Cutscene container to be used solely by Cutscene Branch
-         * It has a cutscene and its respective conditions to be played, as keys
          */
         [Serializable]
         public class CutsceneBranchPath {
-            public Cutscene cutscene;
-            public List<CollectableItem> keys;
+            public Cutscene cutscene; // cutscene to be played
+            public string playOnlyOnceFlag; // if filled, this guarantees cutscene will only play once and then a flag will be saved stopping its repetition
+            public List<CollectableItem> keys; // TODO - transform into conditions, based on save system
+
+            // save when path is played
+            public void SaveProgression() {
+                if (String.IsNullOrEmpty(playOnlyOnceFlag)) {
+                    return;
+                }
+
+                Flags progressionFlag = MMSaveLoadTester.Instance.SaveObject.GetCurrentLevel.progressionFlags.Find(x => x.name == playOnlyOnceFlag);
+                if (progressionFlag == null) {
+                    // create and add new progression flag if it doesn't exist
+                    progressionFlag = new Flags();
+                    progressionFlag.name = playOnlyOnceFlag;
+                    MMSaveLoadTester.Instance.SaveObject.GetCurrentLevel.progressionFlags.Add(progressionFlag);
+                }
+
+                MMSaveLoadTester.Instance.SaveObject.GetCurrentLevel.progressionFlags.Find(x => x.name == playOnlyOnceFlag).done = true;
+                MMSaveLoadTester.Instance.Save();
+            }
+
+            // load if path has been played
+            public bool LoadProgression() {
+                if (String.IsNullOrEmpty(playOnlyOnceFlag)) {
+                    return false;
+                }
+
+                Flags progressionFlag = MMSaveLoadTester.Instance.SaveObject.GetCurrentLevel.progressionFlags.Find(x => x.name == playOnlyOnceFlag);
+                if (progressionFlag == null) {
+                    return false;
+                }
+
+                return progressionFlag.done;
+            }
         }
     }
 }
